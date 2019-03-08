3a81210dca0cfdce3bbf2cee85e2f38a45285947
yoshikingt <yskngt1007@gmail.com>
2019-03-09 02:22:34 +0900

    Initial commit

---
+++ scraping.py
@@ -0,0 +1,28 @@
+import re
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+# ソース取得
+r = requests.get('https://www.llsunshine-numazu.jp/')
+html = r.content
+soup = BeautifulSoup(html, 'html.parser')
+shopList = soup.find_all('dl', class_=re.compile('^shop_st'))
+
+outputList = []
+
+# スタンプ設置箇所ごとに処理
+for shop in shopList:
+    name = shop.dt.text
+    info = shop.dd.text
+    info = info.replace('\n', ' ')
+    info = info.replace('\r', ' ')
+    link = ''
+    if shop.find('a') is not None: # リンクがない場合の対策
+        link = shop.find('a').get('href')
+    outputList.append([name, info, link])
+
+# CSV として書き出し
+with open('output.csv', 'w', encoding='UTF-8') as f:
+    writer = csv.writer(f, lineterminator='\n')
+    writer.writerows(outputList)
