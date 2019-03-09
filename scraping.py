520451dc631707f437e6fbe1436cf31ad7679a02
yoshikingt <yskngt1007@gmail.com>
2019-03-09 12:37:22 +0900

    [modify] CSV ファイルにヘッダ追加
    - 1行目にヘッダを追加
    - CSV ファイル名を変更
    - info 置換処理をリファクタリング

--- scraping.py
+++ scraping.py
@@ -10,19 +10,18 @@ soup = BeautifulSoup(html, 'html.parser')
 shopList = soup.find_all('dl', class_=re.compile('^shop_st'))
 
 outputList = []
+outputList.append(['Name', 'Info', 'Link']) # ヘッダ
 
 # スタンプ設置箇所ごとに処理
 for shop in shopList:
     name = shop.dt.text
-    info = shop.dd.text
-    info = info.replace('\n', ' ')
-    info = info.replace('\r', ' ')
+    info = shop.dd.text.replace('\n', ' ').replace('\r', ' ')
     link = ''
     if shop.find('a') is not None: # リンクがない場合の対策
         link = shop.find('a').get('href')
     outputList.append([name, info, link])
 
 # CSV として書き出し
-with open('output.csv', 'w', encoding='UTF-8') as f:
+with open('shop_list.csv', 'w', encoding='UTF-8') as f:
     writer = csv.writer(f, lineterminator='\n')
     writer.writerows(outputList)
